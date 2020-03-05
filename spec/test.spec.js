66e654aa5c194122ffc9a13347e7f83e8b21cd91
Mzito <you@example.com>
2020-03-05 14:29:29 +0200

    more specs added

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -52,3 +52,58 @@ describe('testing delete visitor', ()=>{
 
 
 })
+
+describe('testing update', ()=>{
+    const axios = require('axios')
+    let server
+    const {addNewVisitor,listAllVisitors,deleteVisitor,deleteVisitors,viewVisitor,updateVisitor} = require('../public/app')
+
+
+    beforeEach(()=>{
+        server = require('../public/index')
+    });
+
+    it('should check status code is 200', async (done)=>{
+        try {
+            const route = await axios.get("http://127.0.0.1:3000/updateVisitor/50")
+            expect(route.status).toEqual(200)
+        } catch (err) {
+            console.log(err)
+        }
+        done()
+    })
+
+})
+
+describe('testing delete all visitor', ()=>{
+    const axios = require('axios')
+    let server
+    const {addNewVisitor,listAllVisitors,deleteVisitor,deleteVisitors,viewVisitor,updateVisitor} = require('../public/app')
+
+
+    beforeEach(()=>{
+        server = require('../public/index')
+    });
+
+    it('should check status code is 200', async (done)=>{
+        try {
+            const route = await axios.get("http://127.0.0.1:3000/deleteVisitors")
+            expect(route.status).toEqual(200)
+        } catch (err) {
+            console.log(err)
+        }
+        done()
+    })
+
+    it('should return empty array after deleting all visitors', async (done)=>{
+        try {
+            const route = await axios.get("http://127.0.0.1:3000/deleteVisitors")
+            expect(route.outputData).toEqual([])
+        } catch (err) {
+            console.log(err)
+        }
+        done()
+    })
+
+
+})
